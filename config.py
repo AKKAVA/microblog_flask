f34ead3902f6b3058ac1d8a8fb2f8c7d09a3446d
Akkava <aleasterxs@mail.ru>
2024-12-13 18:31:11 +0300

    add sqlalchemy support

--- config.py
+++ config.py
@@ -1,4 +1,10 @@
 import os
 
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'The-most-difficult-key-ever-666'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
